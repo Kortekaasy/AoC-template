23e5a07ed7fb802ba74bbd5255ba941f11a52147
Yoep Kortekaas <y.a.m.kortekaas@utwente.nl>
2022-11-30 15:14:35 +0100

    Made input parsing and evaluation functions more general

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,58 @@
-// ========================= Challenge Logic ============================
+use std::fmt::Display;
 
-pub fn part1(input: String) -> String {
-    format!("Unimplemented!")
+// ========================= Challenge Logic ============================
+// Define your own output type here for the `parse_input` function.
+type ParsedInput = ();
+
+pub fn parse_input(input: String) -> ParsedInput {
+    panic!("Input parsing not implemented!")
 }
 
-pub fn part2(input: String) -> String {
-    format!("Unimplemented!")
+pub fn part1(input: &ParsedInput) -> impl Display {
+    format!("Part 1 not implemented!")
+}
+
+pub fn part2(input: &ParsedInput) -> impl Display {
+    format!("Part2 not implemented!")
 }
 
 // =========================== Main Function ============================
 #[allow(dead_code)]
 fn main() {
-    formatted_print("A", part1(read_file("input")));
-    formatted_print("B", part2(read_file("input")));
+    // Parse sample and challenge input
+    let sample = parse_input(read_file("sample"));
+    let input = parse_input(read_file("input"));
+
+    // Part 1
+    // Define sample answer
+    let sample_answer_part_1 = "";
+
+    // Evaluate sample input, and compare with sample answer
+    assert_eq!(format!("{}", part1(&sample)), sample_answer_part_1);
+
+    // If sample input evaluated correctly, print output of part 1 with 
+    // challenge output.
+    formatted_print("1", part1(&input));
+
+    // Part 2
+    // Define sample answer
+    let sample_answer_part_2 = "";
+
+    // Evaluate sample input, and compare with sample answer
+    assert_eq!(format!("{}", part2(&sample)), sample_answer_part_2);
+
+    // If sample input evaluated correctly, print output of part 1 with 
+    // challenge output.
+    formatted_print("2", part2(&input));
+
 }
 
 pub fn read_file(file_name: &str) -> String {
     return std::fs::read_to_string(file_name).expect(format!("File {} not found", file_name).as_str());
 }
 
-fn formatted_print(part: &str, output: String) {
+fn formatted_print<T : Display>(part: &str, output: T) {
     println!("==================== Part {} ======================", part);
-    for l in output.lines() {
-        println!("| {:^46} |", l);
-    }
+    println!("{}", output);
     println!("==================================================");
 }
