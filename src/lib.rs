4b8caaf427c6ae57132489e4499797432569f721
Yoep Kortekaas <y.a.m.kortekaas@utwente.nl>
2022-12-02 12:52:18 +0100

    Added benchmarking of input parsing step

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,13 @@ use std::fmt::Display;
 mod main;
 
 pub fn part1() -> impl Display {
-    let raw_input = include_str!("../input").to_owned();
+    let raw_input = include_str!("../input");
     let parsed_input = main::parse_input(raw_input);
     main::part1(&parsed_input)
 }
 
 pub fn part2() -> impl Display {
-    let raw_input = include_str!("../input").to_owned();
+    let raw_input = include_str!("../input");
     let parsed_input = main::parse_input(raw_input);
     main::part2(&parsed_input)
 }
